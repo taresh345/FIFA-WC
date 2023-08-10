cf4cb2f01c991d1db78f66e96651856035117cc5
taresh345 <48683755+taresh345@users.noreply.github.com>
2023-08-10 16:17:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -268,7 +268,7 @@ if user_menu == 'Historical Comparison and Insights':
     st.header(' Winner, Runners-Up, Third:  top-performing teams in '
               'each World Cup and consistent podium finishers')
     tmp = helper.win_stats(wc)
-    st.write(tmp.columns)
+    # st.write(tmp.columns)
     st.table(tmp)
 
     fig = px.bar(tmp,x="index", y='Winner', color='index' , title="Winners")
