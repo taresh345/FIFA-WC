9729df5dba2fe720d17ae7a691faa693a07ccd69
taresh345 <48683755+taresh345@users.noreply.github.com>
2023-08-10 16:11:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -268,6 +268,7 @@ if user_menu == 'Historical Comparison and Insights':
     st.header(' Winner, Runners-Up, Third:  top-performing teams in '
               'each World Cup and consistent podium finishers')
     tmp = helper.win_stats(wc)
+    st.write(tmp.columns)
 
     fig = px.bar(tmp,x="index", y='count', color='index' , title="Winners")
     st.plotly_chart(fig, use_container_width=True)
